b25cce7c0ab2c6e9806a1dd1eda6c8f72a5668bd
danieldelarosavalero <danieldelarosavalero@gmail.com>
2024-10-01 15:58:57 +0200

    1er commit
    Hello World en vez de Hello CMake

---
+++ prueba_AB_progra/prueba_AB_progra/prueba_AB_progra.cpp
@@ -0,0 +1,12 @@
+﻿// prueba_AB_progra.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "prueba_AB_progra.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World." << endl;
+	return 0;
+}
