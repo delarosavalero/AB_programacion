38a9e6e9d2ecb1d34edfc47980782f539346b27c
danieldelarosavalero <danieldelarosavalero@gmail.com>
2024-11-25 20:33:54 +0100

    creacion de class Paciente también private con strings y public con funcion void y bool

--- prueba_AB_progra/prueba_AB_progra/prueba_AB_progra.cpp
+++ prueba_AB_progra/prueba_AB_progra/prueba_AB_progra.cpp
@@ -1,12 +1,38 @@
 ﻿// prueba_AB_progra.cpp: define el punto de entrada de la aplicación.
 //
-
-#include "prueba_AB_progra.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello World." << endl;
-	return 0;
-}
+class Paciente {
+private:
+	string nombre;
+	string indentificacion;
+	string fechaIngreso;
+	vector<string> historialClinico;
+
+public:
+	Paciente(string nom, string id, string fecha) : nombre(nom), indentificacion(id), fechaIngreso(fecha) {}
+
+
+	void registrarHistorial(string evento) {
+		historialClinico.push_back(evento);
+	}
+
+	void mostrarInformacion() {
+		cout << "Nombre: " << nombre << endl;
+		cout << "ID: " << indentificacion << endl;
+		cout << "Fecha de Ingreso: " << fechaIngreso << endl;
+		cout << "Historial Clinico: " << endl;
+		for (const auto& evento : historialClinico) {
+			cout << "- " << evento << endl;
+		}
+	}
+
+	bool buscarPaciente(string criterio) {
+		return nombre == criterio || indentificacion == criterio;
+	}
+};
+
