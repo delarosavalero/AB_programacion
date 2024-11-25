ceffc2055b8746f75603d3360ec98e0d206aa87a
danieldelarosavalero <danieldelarosavalero@gmail.com>
2024-11-25 21:18:48 +0100

    creacion del int main incluyendo un pequeño ejemplo

--- prueba_AB_progra/prueba_AB_progra/prueba_AB_progra.cpp
+++ prueba_AB_progra/prueba_AB_progra/prueba_AB_progra.cpp
@@ -139,3 +139,17 @@ public:
 		}
 	}
 };
+
+int main() {
+	GestorHospital gestor;
+	gestor.registrarPaciente("Juan Perez", "12345", "2024-11-25");
+	gestor.registrarMedico("Dr. Smith", "Cardiología", "67890");
+
+	gestor.programarCita("2024-11-26 10:00", "12345", "67890", 1);
+
+	gestor.listarPacientes();
+	gestor.listarMedicos();
+	gestor.listarCitas();
+
+	return 0;
+}
