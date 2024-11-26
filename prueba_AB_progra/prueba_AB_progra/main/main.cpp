45ba57564c0898ea1279ff6e2cdc95f5afea6d75
danieldelarosavalero <danieldelarosavalero@gmail.com>
2024-11-26 15:43:11 +0100

    quitar instrucciones

--- prueba_AB_progra/prueba_AB_progra/main/main.cpp
+++ prueba_AB_progra/prueba_AB_progra/main/main.cpp
@@ -7,20 +7,19 @@
 int main() {
     GestorHospital gestor;
 
-    // Cargar datos previamente guardados
     gestor.recuperarSistema();
 
-    // Realizar operaciones en el sistema
+    
     gestor.registrarPaciente("Juan Perez", "12345", "2024-11-25");
     gestor.registrarMedico("Dr. Smith", "Cardiología", "67890");
     gestor.programarCita("2024-11-26 10:00", "12345", "67890", 1);
 
-    // Listar pacientes, médicos y citas
+    
     gestor.listarPacientes();
     gestor.listarMedicos();
     gestor.listarCitas();
 
-    // Guardar los datos después de modificar
+    
     gestor.guardarSistema();
 
     return 0;
