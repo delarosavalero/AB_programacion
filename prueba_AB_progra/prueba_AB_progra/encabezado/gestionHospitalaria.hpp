626714a0742c28ba5ee5796dfa4ae7f82a8cc64d
danieldelarosavalero <danieldelarosavalero@gmail.com>
2024-11-26 15:27:40 +0100

    creacion gestionHospitalaria.hpp

---
+++ prueba_AB_progra/prueba_AB_progra/encabezado/gestionHospitalaria.hpp
@@ -0,0 +1,30 @@
+#ifndef GESTORHOSPITAL_HPP
+#define GESTORHOSPITAL_HPP
+
+#include <vector>
+#include <fstream>
+#include "Paciente.hpp"
+#include "Medico.hpp"
+#include "CitaMedica.hpp"
+
+using namespace std;
+
+class GestorHospital {
+private:
+    vector<Paciente> listaPacientes;
+    vector<Medico> listaMedicos;
+    vector<CitaMedica> listaCitas;
+
+public:
+    void registrarPaciente(string nombre, string id, string fecha);
+    void registrarMedico(string nombre, string especialidad, string id);
+    void programarCita(string fecha, string idPaciente, string idMedico, int prioridad);
+    void listarPacientes() const;
+    void listarMedicos() const;
+    void listarCitas() const;
+
+    void guardarSistema() const;
+    void recuperarSistema();
+};
+
+#endif // G
